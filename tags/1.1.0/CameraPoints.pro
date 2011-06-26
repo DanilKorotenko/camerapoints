35584070f1b8d7dbededfbab69b941c5c535f9af
danil.korotenko <danil.korotenko@888db010-cced-89f6-08ad-e5de05c90518>
2011-06-26 08:33:33 +0000

    Created v 1.1.0 tag.

---
+++ tags/1.1.0/CameraPoints.pro
@@ -0,0 +1,51 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-06-22T00:03:34
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = CameraPoints
+TEMPLATE = app
+
+VERSION = 1.1.0
+
+INCLUDEPATH = Sources \
+	Sources/UI \
+	Sources/DataModel \
+	alglib/src
+
+SOURCES += Sources/main.cpp\
+	Sources/UI/Widget.cpp \
+	alglib/src/statistics.cpp \
+	alglib/src/specialfunctions.cpp \
+	alglib/src/solvers.cpp \
+	alglib/src/optimization.cpp \
+	alglib/src/linalg.cpp \
+	alglib/src/interpolation.cpp \
+	alglib/src/integration.cpp \
+	alglib/src/fasttransforms.cpp \
+	alglib/src/diffequations.cpp \
+	alglib/src/dataanalysis.cpp \
+	alglib/src/ap.cpp \
+	alglib/src/alglibmisc.cpp \
+	alglib/src/alglibinternal.cpp \
+	Sources/DataModel/GeoCoord.cpp
+
+HEADERS  += Sources/UI/Widget.h \
+	alglib/src/stdafx.h \
+	alglib/src/statistics.h \
+	alglib/src/specialfunctions.h \
+	alglib/src/solvers.h \
+	alglib/src/optimization.h \
+	alglib/src/linalg.h \
+	alglib/src/interpolation.h \
+	alglib/src/integration.h \
+	alglib/src/fasttransforms.h \
+	alglib/src/diffequations.h \
+	alglib/src/dataanalysis.h \
+	alglib/src/ap.h \
+	alglib/src/alglibmisc.h \
+	alglib/src/alglibinternal.h \
+	Sources/DataModel/GeoCoord.h
---
+++ tags/1.1.0/Sources/UI/Widget.cpp
@@ -0,0 +1,158 @@
+/*******************************************************************
+ Copyright (C) 2011 Danil Korotenko (danil.korotenko@gmail.com)
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public Licence
+ as published by the Free Software Foundation; either version 3
+ of the Licence, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public Licence for more details.
+
+ You should have received a copy of the GNU General Public Licence
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+********************************************************************/
+
+#include "Widget.h"
+#include "solvers.h"
+#include <math.h>
+#include "GeoCoord.h"
+
+Widget::Widget(QWidget *parent) : QWidget(parent)
+{
+	QGridLayout *topLayout = new QGridLayout(this);
+
+	topLayout->addWidget(new QLabel(tr("camera 1"),this),0,0,1,1);
+
+	QRegExp latitudeRegExp("(-?(?:[0-9]{1,2})) ([0-5]?[0-9]) ([0-5]?[0-9]\\.[0-9]{1,2})");
+	QRegExp longitudeRegExp("(-?(?:(?:1[0-7][0-9])|(?:[0-9]{1,2}))) ([0-5]?[0-9]) ([0-5]?[0-9]\\.[0-9]{1,2})");
+	QRegExp azimuthRegExp("^((\\d|[1-9]\\d|[12]\\d\\d|3[0-5]\\d)(\\.\\d)?|360(\\.0)?)$");
+	latitudeValidator = new QRegExpValidator(latitudeRegExp, this);
+	longitudeValidator = new QRegExpValidator(longitudeRegExp, this);
+	azimuthValidator = new QRegExpValidator(azimuthRegExp, this);
+
+	camera1Latitude = new QLineEdit(this);
+	camera1Latitude->setText("49 02 53.81");
+	camera1Latitude->setValidator(latitudeValidator);
+	topLayout->addWidget(new QLabel(tr("Latitude:"),this),1,0,1,1);
+	topLayout->addWidget(camera1Latitude,1,1,1,1);
+
+	camera1Longitude = new QLineEdit(this);
+	camera1Longitude->setText("38 13 12.75");
+	camera1Longitude->setValidator(longitudeValidator);
+	topLayout->addWidget(new QLabel(tr("Longitude:"),this),1,2,1,1);
+	topLayout->addWidget(camera1Longitude,1,3,1,1);
+
+	camera1Azimuth = new QLineEdit(this);
+	camera1Azimuth->setText("136.7");
+	camera1Azimuth->setValidator(azimuthValidator);
+	topLayout->addWidget(new QLabel(tr("Azimuth:"),this),1,4,1,1);
+	topLayout->addWidget(camera1Azimuth,1,5,1,1);
+
+	topLayout->addWidget(new QLabel(tr("camera 2"),this),2,0,1,1);
+
+	camera2Latitude = new QLineEdit(this);
+	camera2Latitude->setText("49 02 49.26");
+	camera2Latitude->setValidator(latitudeValidator);
+	topLayout->addWidget(new QLabel(tr("Latitude:"),this),3,0,1,1);
+	topLayout->addWidget(camera2Latitude,3,1,1,1);
+
+	camera2Longitude = new QLineEdit(this);
+	camera2Longitude->setText("38 13 06.37");
+	camera2Longitude->setValidator(longitudeValidator);
+	topLayout->addWidget(new QLabel(tr("Longitude:"),this),3,2,1,1);
+	topLayout->addWidget(camera2Longitude,3,3,1,1);
+
+	camera2Azimuth = new QLineEdit(this);
+	camera2Azimuth->setText("100.9");
+	camera2Azimuth->setValidator(azimuthValidator);
+	topLayout->addWidget(new QLabel(tr("Azimuth:"),this),3,4,1,1);
+	topLayout->addWidget(camera2Azimuth,3,5,1,1);
+
+	topLayout->addWidget(new QLabel(tr("point"),this),4,0,1,1);
+
+	pointLatitude = new QLineEdit(this);
+	pointLatitude->setReadOnly(true);
+	topLayout->addWidget(new QLabel(tr("Latitude:"),this),5,0,1,1);
+	topLayout->addWidget(pointLatitude,5,1,1,1);
+
+	pointLongitude = new QLineEdit(this);
+	pointLongitude->setReadOnly(true);
+	topLayout->addWidget(new QLabel(tr("Longitude:"),this),5,2,1,1);
+	topLayout->addWidget(pointLongitude,5,3,1,1);
+
+	QPushButton *solveButton = new QPushButton(tr("Solve"),this);
+	connect(solveButton,SIGNAL(clicked()),SLOT(slotSolve()));
+	topLayout->addWidget(solveButton,5,5,1,1);
+
+	new QShortcut(Qt::Key_Return, this, SLOT(slotSolve()));
+	new QShortcut(Qt::Key_Escape, this, SLOT(close()));
+
+	this->setLayout(topLayout);
+}
+
+Widget::~Widget()
+{
+
+}
+
+void Widget::slotSolve()
+{
+	qreal angle1 = camera1Azimuth->text().toFloat();
+	angle1 = angle1 * (M_PI / 180);
+	qreal cosAngle1 = qCos(angle1);
+	qreal sinAngle1 = qSin(angle1);
+	qreal angle2 = camera2Azimuth->text().toFloat();
+	angle2 = angle2 * (M_PI / 180);
+	qreal cosAngle2 = qCos(angle2);
+	qreal sinAngle2 = qSin(angle2);
+
+	GeoCoord lat1;
+	lat1.setString(camera1Latitude->text());
+	GeoCoord lat2;
+	lat2.setString(camera2Latitude->text());
+
+	qreal x1 = lat1.getDouble();
+	qreal x2 = lat2.getDouble();
+
+	GeoCoord longitude1;
+	longitude1.setString(camera1Longitude->text());
+	GeoCoord longitude2;
+	longitude2.setString(camera2Longitude->text());
+
+	qreal y1 = longitude1.getDouble();
+	qreal y2 = longitude2.getDouble();
+
+	QString matrix = QString("[[%1,%2],[%3,%4]]")
+		.arg(cosAngle1).arg(-cosAngle2).arg(sinAngle1).arg(-sinAngle2);
+
+	QString vector = QString("[%1,%2]").arg(x2-x1).arg(y2-y1);
+
+	alglib::real_2d_array matr(matrix.toAscii().data());
+	alglib::real_1d_array vec(vector.toAscii().data());
+	alglib::real_1d_array res;
+
+	alglib::ae_int_t info;
+	alglib::densesolverreport rep;
+	alglib::rmatrixsolve(matr,2,vec,info,rep,res);
+
+	std::string resStr = res.tostring(1);
+
+	qreal r1 = res[0];
+
+	qreal px = r1 * qCos(angle1) + x1;
+	qreal py = r1 * qSin(angle1) + y1;
+
+	GeoCoord plat;
+	plat.setDouble(px);
+
+	GeoCoord plongi;
+	plongi.setDouble(py);
+
+	pointLatitude->setText(plat.getString());
+	pointLongitude->setText(plongi.getString());
+
+}
